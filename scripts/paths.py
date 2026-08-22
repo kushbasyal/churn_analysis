510d1f096fb99982ccb7dda9858056d98b035dd7
kushal <kushalbasyal123@gmail.com>
2026-08-22 16:17:54 +1000

    Added visualization_dir on config.json and paths given

---
+++ scripts/paths.py
@@ -0,0 +1,22 @@
+import os
+import json
+
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+config_path = os.path.join(project_root, "config.json")
+
+with open(config_path, "r") as f:
+    config = json.load(f)
+
+raw_data_path = os.path.join(project_root, config["Data"]["raw_data_path"])
+
+clean_data_path = os.path.join(project_root,config["Data"]["clean_data_path"])
+
+visualization_dir = os.path.join(project_root, config["Data"]["visualization_dir"])
+os.makedirs(visualization_dir,exist_ok=True)
+
+
+if __name__ == '__main__':
+    print("Raw Data Path:", raw_data_path)
+    print("Clean Data Path:", clean_data_path)
+    print("Visualization Path:", visualization_dir)
